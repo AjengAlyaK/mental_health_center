93e0cf4e5152782a3b39c84d3e7da34fc8b1b47b
Ajeng Alya Kartika Sari <ajenga990@gmail.com>
2023-08-30 22:08:36 +0700

    add bug :v

--- resources/views/design/makeAnAppointment.blade.php
+++ resources/views/design/makeAnAppointment.blade.php
@@ -28,34 +28,49 @@
 
                             <div class="col-md-6 col-sm-6">
                                     <label for="email">Email</label>
-                                    {{-- <input value="{{$id->email}}"style="border:1px solid black" autocomplete="off" type="email" class="form-control" id="email" name="email"> --}}
+                                    @if ($id)
+                                        <input value="{{$id->email}}"style="border:1px solid black" autocomplete="off" type="email" class="form-control" id="email" name="email">
+                                    @else
+                                        <input value=""style="border:1px solid black" autocomplete="off" type="email" class="form-control" id="email" name="email">
+                                    @endif
                             </div>
 
                             <div class="col-md-6 col-sm-6">
                                     <label for="date">Tanggal</label>
-                                    {{-- <input style="border:1px solid black" autocomplete="off" type="date" name="tanggal" value="" class="form-control"> --}}
+                                    <input style="border:1px solid black" autocomplete="off" type="date" name="tanggal" value="" class="form-control">
                             </div>
 
                             <div class="col-md-6 col-sm-6">
                                     <label for="select">Dokter</label>
                                     <select name="dokter" class="form-control" style="border:1px solid black">
-                                        {{-- @foreach ($doctor as $d)
+                                        @foreach ($doctor as $d)
                                         <option value="{{$d->name}}">{{$d->name}}</option>
-                                        @endforeach --}}
+                                        @endforeach
                                     </select>
                             </div>
 
                             <div class="col-md-6 col-sm-6" hidden>
-                                
-                                {{-- <input hidden autocomplete="off" type="number" class="form-control" id="email" name="user_id" value="{{$id->id}}"> --}}
+                                @if ($id)
+                                    <input hidden autocomplete="off" type="number" class="form-control" id="email" name="user_id" value="{{$id->id}}">
+                                @else
+                                    <input hidden autocomplete="off" type="number" class="form-control" id="email" name="user_id" value="">
+                                @endif
                             </div>
 
                             <div class="col-md-12 col-sm-12">
                                     <label for="telephone">Nomor Handphone</label>
-                                    {{-- <input value="{{$id->phone}}" style="border:1px solid black" autocomplete="off" type="tel" class="form-control" id="phone" name="phone"> --}}
+                                    @if ($id)
+                                        <input value="{{$id->phone}}" style="border:1px solid black" autocomplete="off" type="tel" class="form-control" id="phone" name="phone">
+                                    @else
+                                        <input value="" style="border:1px solid black" autocomplete="off" type="tel" class="form-control" id="phone" name="phone">
+                                    @endif
                                     <label for="Message">Pesan Tambahan</label>
                                     <textarea style="border:1px solid black" class="form-control" rows="5" id="message" name="pesan"></textarea>
-                            <button type="submit" class="form-control" id="cf-submit" name="submit">Submit</button>
+                            @if (Auth::user())
+                                <button type="submit" class="form-control" id="cf-submit" name="submit">Submit</button>
+                            @else
+                                <a href="/login" type="submit" class="form-control" id="cf-submit" name="submit">Submit</a>
+                            @endif
                             </div>
                         </div>
                     </form>
